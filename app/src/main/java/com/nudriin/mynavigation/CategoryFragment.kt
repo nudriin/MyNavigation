8ee42dfc0dc15e0b00e5d6dd1bdb6efe8ec13b1d
Nurdin <mrsunnysummer@gmail.com>
2024-10-07 13:59:56 +0700

    feat: create detail category navigation and send data with bundle

--- app/src/main/java/com/nudriin/mynavigation/CategoryFragment.kt
+++ app/src/main/java/com/nudriin/mynavigation/CategoryFragment.kt
@@ -5,13 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.nudriin.mynavigation.databinding.FragmentCategoryBinding
 
 class CategoryFragment : Fragment() {
+    private var _binding: FragmentCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        val EXTRA_NAME = "extra_name"
+        val EXTRA_STOCK = "extra_stock"
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCategoryLifestyle.setOnClickListener {
+            val mBundle = Bundle()
+            mBundle.putString(EXTRA_NAME, "Lifestyle")
+            mBundle.putLong(EXTRA_STOCK, 7)
+
+            Navigation.findNavController(it).navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
+            // navigate dengan mengirimkan data bundle
+//            it.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
